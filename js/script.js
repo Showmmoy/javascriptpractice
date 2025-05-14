8a4a56969f8a37b31a1e683ef37ca520bf5b34da
Showmmoy Barua <128532264+Showmmoy@users.noreply.github.com>
2025-05-14 05:35:11 +0100

    added multiple object

--- js/script.js
+++ js/script.js
@@ -91,3 +91,119 @@ $(document).ready(function(){
             $('.ans').slideToggle(1000);
         });
     });
+
+$(document).ready(function(){
+    $( function() {
+        $( "#draggable" ).draggable();
+      } );
+      $( function() {
+        $( "#accordion" ).accordion({
+          collapsible: true
+        });
+      } );
+    });
+
+    $(document).ready(function(){
+    var data = ["HTML", 
+        "CSS", 
+        "JavaScript", 
+        "PHP", 
+        "Python"
+    ];
+    $( "#topic" ).autocomplete({
+        source: data
+      });
+
+    });
+
+    $(document).ready(function(){
+        $('.counter').counterUp({
+            delay: 10,
+            time: 1000
+        });
+    });
+
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       10,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+
+
+      AOS.init({
+        duration: 1200,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false,
+        anchorPlacement: 'top-bottom',
+      });
+
+        $(document).ready(function(){
+           /**
+     * Demo 1
+     */
+    var arc = function (t)
+    {
+        return {
+            x: 500.0 + 300.0 * Math.cos(2.0 * Math.PI * t - 0.5 * Math.PI),
+            y: 400.0 + 300.0 * Math.sin(2.0 * Math.PI * t - 0.5 * Math.PI)
+        };
+    };
+
+    $('#text1').curvedText({
+        curve:    arc,
+        domain:   [-0.2, 0.2],
+        viewport: { x: 0.0, y: 0.0, width: 1000.0, height: 400.0 }
+    });  
+        })
+
+    $(document).ready(function(){
+    var swiper = new Swiper(".mySwiper", {
+      effect: "cards",
+      grabCursor: true,
+    });
+
+    });
+
+    $(document).ready(function(){
+
+        $('.flip-demo').on('done', function() {
+        console.log('doooooonnnnnee!');
+        });
+
+        $('.flip-demo').on('before<a href="https://www.jqueryscript.net/tags.php?/Flip/">Flip</a>ping', function(e, data) {
+        console.log('beforeFlipping:', data);
+        });
+
+        $('.flip-demo').on('afterFlipping', function(e, data) {
+        console.log('afterFlipping:', data);
+        });
+    })
+  
+
+    
+        $(function() {
+            $("#marqueeLeft").marquee();
+            
+            $("#marqueeRight").marquee({
+                direction: 'right',
+                speed: 15,
+                spaceBetween: 15,
+                pausehover: true
+            });
+            
+            $("#marqueeUP").marquee({
+                direction: 'up',
+                pausehover: true
+            });
+
+            $("#marqueeDown").marquee({
+                direction: 'down'
+            })
+        });
+   
